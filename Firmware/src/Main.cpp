804f6609802b293eb9509ace22095e1490869a51
Brennan Long <brennan.long04@gmail.com>
2026-04-03 18:55:36 -0400

    initial setup of codebase

---
+++ Firmware/src/Main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "Liver-Monitor-A06";
+const char* password = "perfusion-tech";
+
+AsyncWebServer server(80);
+
+// Just copy-paste your HTML from the app folder into this string
+const char index_html[] PROGMEM = R"rawliteral(
+  )rawliteral";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password);
+  
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html);
+  });
+
+  server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+    StaticJsonDocument<200> doc;
+    doc["pressure"] = analogRead(32); // Reading your capacitance sensor
+    String response;
+    serializeJson(doc, response);
+    request->send(200, "application/json", response);
+  });
+
+  server.begin();
+}
+
+void loop() {}
