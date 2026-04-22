1ce04df19bc95625346717e13bac72c44cddeac9
Brennan Long <brennan.long04@gmail.com>
2026-04-22 18:08:40 -0400

    added logo, live telemtry looks cleaner, and stores last 30 seconds of data.

--- Firmware/src/Main.cpp
+++ Firmware/src/Main.cpp
@@ -22,11 +22,12 @@ void setup() {
 
   server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
     StaticJsonDocument<200> doc;
-    doc["pressure"] = analogRead(32); // Reading your capacitance sensor
+    doc["portal_pressure"] = myPortalValue;   // Real sensor variable
+    doc["hepatic_pressure"] = myHepaticValue; // Real sensor variable
     String response;
     serializeJson(doc, response);
     request->send(200, "application/json", response);
-  });
+});
 
   server.begin();
 }
